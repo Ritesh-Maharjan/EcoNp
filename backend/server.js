12fdee90571d93108d8ecad445cb05eb4d009c20
Ritesh <riteshmaharjan07@gmail.com>
2023-01-10 15:58:24 -0800

    Added cors

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const app = require("./app");
+const cors = require('cors')
 const mongoose = require("mongoose")
 require("dotenv").config();
 const connect = require("./config/dbConn");
@@ -7,6 +8,7 @@ const port = process.env.PORT || 5000
 // Connecting to mongo db
 connect()
 
+app.use(cors())
 // when mongoose is connected
 mongoose.connection.once("open", () => {
     console.log("Mongo DB connected");
