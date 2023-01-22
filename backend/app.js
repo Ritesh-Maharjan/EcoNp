616bc3b488b9144d29bc07b5e0a45a0af7b5bb1a
Ritesh <riteshmaharjan07@gmail.com>
2023-01-22 09:16:10 -0800

    Fixed css mistakes

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const app = express();
-const cors = require("cors")
+const cors = require("cors");
+const path = require("path");
 const ErrorHandler = require("./midlleware/error");
 
 app.use(express.json());
-app.use(cors())
+app.use(cors());
+app.use(express.static(path.join(__dirname, "client/build")));
+
 // route imports
 const product = require("./route/product");
 const user = require("./route/user");
